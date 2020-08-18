0afbe8632bb73be1e036d1a94e3318d99db897b7
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-18 19:59:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Add your code here
+class Dog
+  attr_accessor :name
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self #push the object into the all array
+end
+
+def self.all
+  @@all   
+end
+
+def self.print_all
+  @@all.each do | dogobject |
+    dogobject.name
+
+
+end
